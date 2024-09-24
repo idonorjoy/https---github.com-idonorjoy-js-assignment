417a7db4c2d2f179631e853bf1f12f27b3a05bb6
Idonor joy <idonorjoyce@gmail.com>
2024-09-24 12:16:02 +0100

    assignment

---
+++ test.js
@@ -0,0 +1,28 @@
+// Arrow function to calculate the area of a rectangle
+/*const calculateArea = (length, width) => {
+    return length * width;
+  };
+  
+  // Define the dimensions of the rectangle
+  const length = 5;
+  const width = 3;
+  
+  // Calculate the area using the arrow function
+  const area = calculateArea(length, width);
+ */
+
+
+  function area_of_rectangle(length,breadth){
+
+    let area = length * breadth
+
+    return area
+    
+  }
+
+  
+
+  console.log(area_of_rectangle(2,7
+
+
+  ))
